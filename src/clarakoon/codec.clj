0aa7ac38b97e02259e6e040f98a36032a9e0f323
Jan Doms <jan.doms@gmail.com>
2013-10-06 16:47:18 +0200

    refactor protocol definition, implemented most methods

--- src/clarakoon/codec.clj
+++ src/clarakoon/codec.clj
@@ -18,20 +18,77 @@
   (swap! decode-with (fn [_] decoder)))
 
 (def commands
-  {:who-master {:code 2
-                :args []
-                :return-type :string-option}
-   :set {:code 9
-         :args [:string :string]
-         :return-type :unit}
-   :exists {:code 7
-            :args [:bool :string]
-            :return-type :bool}})
+  {
+   :ping [1
+          []
+          :unit]
+   :who-master [2
+                []
+                :string-option]
+   :exists [7
+            [:bool :string]
+            :bool]
+   :get [8
+         [:bool :string]
+         :string]
+   :set [9
+         [:string :string]
+         :unit]
+   :delete [0x0a
+            [:string]
+            :unit]
+   :range [0x0b
+           [:string-option :bool :string-option :bool :int32]
+           :string-list]
+   :prefix-keys [0x0c
+                 [:bool :string :int32]
+                 :string-list]
+   :test-and-set [0x0d
+                  [:string :string :string]
+                  :string-option]
+   #_:last-entries #_{:code 0x0e}
+   :range-entries [0x0f
+                   [:bool :string-option :bool :string-option :bool :int32]
+                   :string-string-list]
+   #_:sequence
+   :multi-get [0x11
+               [:bool :string-list]
+               :string-option-list]
+   :expect-progress-possible [0x12
+                              []
+                              :bool]
+   :user-function [0x15
+                   [:string :string-option]
+                   :string-option]
+   :assert [0x16
+            [:string :string-option]
+            :unit]
+   :get-key-count [0x1a
+                   []
+                   :int64]
+   :confirm [0x1b
+             [:string :string]
+             :unit]
+   :rev-range-entries [0x23
+                       [:string-option
+                        :bool
+                        :string-option
+                        :bool
+                        :int32]
+                        :string-string-list]
+   #_:synced-sequence #_[0x24]
+   :delete-prefix [0x27
+                   [:string]
+                   :int32]
+   })
 
 (defn write-arg [buf arg-type arg]
   ((case arg-type
     :string h/buf-write-string
-    :bool h/buf-write-bool) buf arg))
+    :bool h/buf-write-bool
+    :string-option h/buf-write-string-option
+    :int32 h/buf-write-int32
+    :string-list h/buf-write-string-list) buf arg))
 
 (defn write-args [buf arg-types arguments]
   (doall
@@ -40,11 +97,18 @@
 (defn read-response [return-type]
   (fn [buf]
     (let [return-code (.readInt buf)]
-      (case return-code
-        0 (case return-type
-            :unit (h/buf-read-unit buf)
-            :bool (h/buf-read-bool buf)
-            :string-option (h/buf-read-string-option buf))
+      (if (= return-code 0)
+        (let [read-f (case return-type
+                       :unit h/buf-read-unit
+                       :bool h/buf-read-bool
+                       :string-option h/buf-read-string-option
+                       :string h/buf-read-string
+                       :string-list h/buf-read-string-list
+                       :string-string-list h/buf-read-string-string-list
+                       :string-option-list h/buf-read-string-option-list
+                       :int32 h/buf-read-int32
+                       :int64 h/buf-read-int64)]
+          (read-f buf))
         (let [error-string (h/buf-read-string buf)
               error-code (case return-code
                            1 :error-no-magic
@@ -58,7 +122,7 @@
           (list error-code error-string))))))
 
 (defn send-command [client command & arguments]
-  (let [{:keys [code args return-type]} (commands command)
+  (let [[code args return-type] (commands command)
         request-buffer (command-buffer code)]
     (write-args request-buffer args arguments)
     (swap-decode (:decode-with client) (read-response return-type))
--- src/clarakoon/codec/helper.clj
+++ src/clarakoon/codec/helper.clj
@@ -35,8 +35,31 @@
 (defn buf-read-string-option [buf]
   (buf-read-option buf buf-read-string))
 
+(defn buf-read-int32 [buf]
+  (.readInt buf))
 
-(defn buf-write-int [buf i]
+(defn buf-read-int64 [buf]
+  (.readLong buf))
+
+(defn buf-read-list [buf read-value]
+  (let [length (buf-read-int32 buf)]
+    (loop [i length acc []]
+      (if (zero? i)
+        acc
+        (recur (dec i) (conj acc (read-value buf)))))))
+
+(defn buf-read-string-list [buf]
+  (buf-read-list buf buf-read-string))
+
+(defn buf-read-string-option-list [buf]
+  (buf-read-list buf buf-read-string-option))
+
+(defn buf-read-string-string-list [buf]
+  (buf-read-list
+   buf
+   (fn [buf] [(buf-read-string buf) (buf-read-string buf)])))
+
+(defn buf-write-int32 [buf i]
   (.writeInt buf i))
 
 (defn buf-write-bool [buf value]
@@ -45,50 +68,21 @@
 (defn buf-write-string [buf ^String s]
   (buf-write-int buf (.length s))
   (.writeBytes buf (.getBytes s)))
-(ns clarakoon.codec
-  (:import
-   [java.nio.charset
-    Charset]
-   [org.jboss.netty.buffer
-    ChannelBuffers]
-   [java.nio
-    ByteOrder]
-))
 
-(defn new-buffer []
-  (ChannelBuffers/dynamicBuffer ByteOrder/LITTLE_ENDIAN 256))
+(defn buf-write-option [buf write-value value-option]
+  (if (nil? value-option)
+    (.writeByte buf 0x00)
+    (do
+      (.writeByte buf 0x01)
+      (write-value buf value-option))))
 
+(defn buf-write-list [buf write-value values]
+  (buf-write-int32 buf (count values))
+  (doseq [v values]
+    (write-value buf v)))
 
-(defn buf-read-string [buf]
-  (let [length (.readInt buf)
-        value (.toString (.readSlice buf length) (Charset/forName "UTF-8"))]
-    value))
+(defn buf-write-string-option [buf value]
+  (buf-write-option buf-write-string value))
 
-(defn buf-read-option [buf read-value]
-  (let [none-or-some-byte (.readByte buf)
-        result            (case none-or-some-byte
-                            0x00 (list :none)
-                            0x01 (list :some (read-value buf)))]
-    result))
-
-(defn buf-read-bool [buf]
-  (case (.readByte buf)
-    0x00 false
-    0x01 true))
-
-(defn buf-read-unit [buf]
-  :unit)
-
-(defn buf-read-string-option [buf]
-  (buf-read-option buf buf-read-string))
-
-
-(defn buf-write-int [buf i]
-  (.writeInt buf i))
-
-(defn buf-write-bool [buf value]
-  (.writeByte buf (if value 1 0)))
-
-(defn buf-write-string [buf ^String s]
-  (buf-write-int buf (.length s))
-  (.writeBytes buf (.getBytes s)))
+(defn buf-write-string-list [buf values]
+  (buf-write-list buf buf-write-string values))
